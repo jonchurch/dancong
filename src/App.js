0c0c47fc5f469232290cb455fd1360303c96485e
jonchurch <me@jonchurch.com>
2017-07-17 23:52:06 -0400

    ignore .env

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import FacebookLogin from 'react-facebook-login'
+
 import './App.css';
+
+const responseFacebook = (res) => {
+	console.log('=======FB RES', res)
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+		<FacebokLogin
+			appId=process.env.APP_ID
+			autoLoad={true}
+			fields="name,email,manage_pages"
+			callback={responseFacebook} />
+		{/* document.getElementById('')	*/}
       </div>
     );
   }
