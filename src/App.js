c6ce17e31f5d3cca0277ecb88d9591544d5ef4a3
jonchurch <me@jonchurch.com>
2017-07-18 02:27:46 -0400

    get page list, populate selector with pages

--- src/App.js
+++ src/App.js
@@ -7,16 +7,26 @@ import './App.css';
 const fb = new Facebook()
 
 class App extends Component {
+	constructor() {
+		super()
+
+		this.responseFacebook = this.responseFacebook.bind(this)
+	}
 
 async responseFacebook (res) {
 	console.log('=======FB RES', res)
 	const accounts = await fb.api('me/accounts', { access_token: res.accessToken })
-			
-	console.log('Pages:',accounts.data.length)
+	// now that I have the pages list
+	// user needs to select their page to manage
+	const pages = accounts.data
+	this.setState({ pages })
 	
 }
+
+state = {
+	pages: []
+}
   render() {
-	  console.log(process.env.APP_ID)
     return (
       <div className="App">
 		<FacebookLogin
@@ -25,7 +35,22 @@ async responseFacebook (res) {
 			fields="name,email"
 			scope="manage_pages"
 			callback={this.responseFacebook} />
-		{/* document.getElementById('')	*/}
+		<div className="page-selector">
+			<form>
+			<select name="page">
+				{
+					this.state.pages.map((val) => {
+							return (
+							<option key={val.id} value={val.id}>{val.name}</option>
+						)
+					})
+				}
+			
+				
+		</select>
+		
+			</form>
+		</div>
       </div>
     );
   }
