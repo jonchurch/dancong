ccd72307887684571491ece22dc29f923ba5ce74
jonchurch <me@jonchurch.com>
2017-07-22 17:45:46 -0400

    add multi-hears branch, greet test works

--- server/bot/skills/test.js
+++ server/bot/skills/test.js
@@ -4,13 +4,15 @@ module.exports = (controller)=> {
 	controller.hears('^hi$', 'message_received', (bot, message) => {
 		// find the config we need for this handler
 		const greeter = bot.config.bots.greeter
-		console.log('=======BOT CONFIG\n', typeof bot.config.bots)
-		console.log({greeter})
 		// if greeing bot is not setup or deactivated, return true so the rest of the triggers will be evaluated
 		if (! greeter || ! greeter.active) {
 			return true
 		}
-		console.log('bot stuff', bot.config.bots)
 		bot.reply(message, greeter.config.greeting)
+	})
+
+
+	controller.hears('(.*)', 'message_received', (bot, message) => {
+		bot.reply(message, 'I do not understand')
 	})
 }
