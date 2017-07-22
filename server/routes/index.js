b834d4f3775ed8e03c36b0a8e5f3b179e7961564
jonchurch <me@jonchurch.com>
2017-07-22 00:37:45 -0400

    await all config calls, then loop through messages spawning bots

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,8 +10,6 @@ const configController = require('../controllers/configController')
 const botController = require('../controllers/botController')
 const pageController = require('../controllers/pageController')
 
-
-
 module.exports = (controller) => {
 
 // Get Config for react app
@@ -32,8 +30,7 @@ router.post('/page/:id', catchErrors(pageController.createPage))
 router.get('/page/:id', catchErrors(pageController.getPageById))
 router.get('/pages', catchErrors(pageController.getAllPages))
 
-
-
+// bot webhook
 router.post('/facebook/receive', function(req, res) {
 
 	// NOTE: we should enforce the token check here
@@ -59,161 +56,138 @@ router.get('/facebook/receive', function(req, res) {
 });
 
 const getConfig = async (obj) => {
-	let data = {}
+	let data = []
 
-	await Promise.all(obj.entry.map(page => {
-		data[page.id] = await rp.get(`${api_root}/page/${page.id}`)
+	await Promise.all(obj.entry.map(async page => {
+		const d = await rp.get(`${api_root}/page/${page.id}`)
+		data.push(d)
 	}))
+	return data
 }
 
 
 const handleWebhookPayload = async (req, res) => {
 
 
-        var obj = req.body;
-		let pageConfigPromises = []
-		const botConfigs = getConfig(obj)
+	var obj = req.body;
+	if (obj.entry) {
 
-        if (obj.entry) {
-            for (var e = 0; e < obj.entry.length; e++) {
-				// spawn configed bot for this page!
-				
-				const page_id = obj.entry[e].id
-				console.log({page_id})
-				
-				// oh yah, loops and promises
-				pageConfigPromises.push(rp.get(`${api_root}/page/${page_id}`))
-			}
-			
-			Promise.all(pageConfigPromises).then((pageConfigs) => {
+	const botConfigs = await getConfig(obj)
+	console.log({botConfigs})
+		for (var e = 0; e < obj.entry.length; e++) {
+			// spawn configed bot for this page!
+			const config = botConfigs.find(el => el.id === obj.entry[e].id)
+			console.log({config})
+			const bot = controller.spawn(config)
 
-			console.log('BUT YOU PROMISED',pageConfigs[0])
+			for (var m = 0; m < obj.entry[e].messaging.length; m++) {
+				var facebook_message = obj.entry[e].messaging[m];
+				if (facebook_message.message) {
+					var message = {
+						text: facebook_message.message.text,
+						user: facebook_message.sender.id,
+						channel: facebook_message.sender.id,
+						page: facebook_message.recipient.id,
+						timestamp: facebook_message.timestamp,
+						seq: facebook_message.message.seq,
+						is_echo: facebook_message.message.is_echo,
+						mid: facebook_message.message.mid,
+						sticker_id: facebook_message.message.sticker_id,
+						attachments: facebook_message.message.attachments,
+						quick_reply: facebook_message.message.quick_reply,
+						type: 'user_message',
+					};
 
+					controller.receiveMessage(bot, message);
+				} else if (facebook_message.postback) {
 
-            for (var e = 0; e < obj.entry.length; e++) {
-				// spawn configed bot for this page!
-				const config = pageConfigPromises.find((el) => 
-					{
-						console.log('EL IN FIND;',el)
-						return el.id == obj.entry[e].id
-					})
-				if (! config ) {
-					console.log({config})
-					console.log('Cant find bot config!')
-				console.log('I DID PROMISE',pageConfigs[0])
-					return
+					// trigger BOTH a facebook_postback event
+					// and a normal message received event.
+					// this allows developers to receive postbacks as part of a conversation.
+					var message = {
+						text: facebook_message.postback.payload,
+						payload: facebook_message.postback.payload,
+						user: facebook_message.sender.id,
+						channel: facebook_message.sender.id,
+						page: facebook_message.recipient.id,
+						timestamp: facebook_message.timestamp,
+						referral: facebook_message.postback.referral,
+					};
+
+					controller.trigger('facebook_postback', [bot, message]);
+
+					if (controller.config.receive_via_postback) {
+						var message = {
+							text: facebook_message.postback.payload,
+							user: facebook_message.sender.id,
+							channel: facebook_message.sender.id,
+							page: facebook_message.recipient.id,
+							timestamp: facebook_message.timestamp,
+							type: 'facebook_postback',
+							referral: facebook_message.postback.referral,
+						};
+
+						controller.receiveMessage(bot, message);
+					}
+
+				} else if (facebook_message.optin) {
+
+					var message = {
+						optin: facebook_message.optin,
+						user: facebook_message.sender.id,
+						channel: facebook_message.sender.id,
+						page: facebook_message.recipient.id,
+						timestamp: facebook_message.timestamp,
+					};
+
+					controller.trigger('facebook_optin', [bot, message]);
+				} else if (facebook_message.delivery) {
+
+					var message = {
+						delivery: facebook_message.delivery,
+						user: facebook_message.sender.id,
+						channel: facebook_message.sender.id,
+						page: facebook_message.recipient.id
+					};
+
+					controller.trigger('message_delivered', [bot, message]);
+				} else if (facebook_message.read) {
+
+					var message = {
+						read: facebook_message.read,
+						user: facebook_message.sender.id,
+						channel: facebook_message.sender.id,
+						page: facebook_message.recipient.id,
+						timestamp: facebook_message.timestamp,
+					};
+
+					controller.trigger('message_read', [bot, message]);
+				} else if (facebook_message.referral) {
+					var message = {
+						user: facebook_message.sender.id,
+						channel: facebook_message.sender.id,
+						page: facebook_message.recipient.id,
+						timestamp: facebook_message.timestamp,
+						referral: facebook_message.referral,
+					};
+
+					controller.trigger('facebook_referral', [bot, message]);
+				} else if (facebook_message.account_linking) {
+					var message = {
+						user: facebook_message.sender.id,
+						channel: facebook_message.sender.id,
+						timestamp: facebook_message.timestamp,
+						account_linking: facebook_message.account_linking,
+					};
+
+					controller.trigger('facebook_account_linking', [bot, message]);
+				}  else {
+					controller.log('Got an unexpected message from Facebook: ', facebook_message);
 				}
-				const bot = controller.spawn(config)
 			}
-
-                for (var m = 0; m < obj.entry[e].messaging.length; m++) {
-                    var facebook_message = obj.entry[e].messaging[m];
-                    if (facebook_message.message) {
-                        var message = {
-                            text: facebook_message.message.text,
-                            user: facebook_message.sender.id,
-                            channel: facebook_message.sender.id,
-                            page: facebook_message.recipient.id,
-                            timestamp: facebook_message.timestamp,
-                            seq: facebook_message.message.seq,
-                            is_echo: facebook_message.message.is_echo,
-                            mid: facebook_message.message.mid,
-                            sticker_id: facebook_message.message.sticker_id,
-                            attachments: facebook_message.message.attachments,
-                            quick_reply: facebook_message.message.quick_reply,
-                            type: 'user_message',
-                        };
-
-                        controller.receiveMessage(bot, message);
-                    } else if (facebook_message.postback) {
-
-                        // trigger BOTH a facebook_postback event
-                        // and a normal message received event.
-                        // this allows developers to receive postbacks as part of a conversation.
-                        var message = {
-                            text: facebook_message.postback.payload,
-                            payload: facebook_message.postback.payload,
-                            user: facebook_message.sender.id,
-                            channel: facebook_message.sender.id,
-                            page: facebook_message.recipient.id,
-                            timestamp: facebook_message.timestamp,
-                            referral: facebook_message.postback.referral,
-                        };
-
-                        controller.trigger('facebook_postback', [bot, message]);
-
-                        if (controller.config.receive_via_postback) {
-                            var message = {
-                                text: facebook_message.postback.payload,
-                                user: facebook_message.sender.id,
-                                channel: facebook_message.sender.id,
-                                page: facebook_message.recipient.id,
-                                timestamp: facebook_message.timestamp,
-                                type: 'facebook_postback',
-                                referral: facebook_message.postback.referral,
-                            };
-
-                            controller.receiveMessage(bot, message);
-                        }
-
-                    } else if (facebook_message.optin) {
-
-                        var message = {
-                            optin: facebook_message.optin,
-                            user: facebook_message.sender.id,
-                            channel: facebook_message.sender.id,
-                            page: facebook_message.recipient.id,
-                            timestamp: facebook_message.timestamp,
-                        };
-
-                        controller.trigger('facebook_optin', [bot, message]);
-                    } else if (facebook_message.delivery) {
-
-                        var message = {
-                            delivery: facebook_message.delivery,
-                            user: facebook_message.sender.id,
-                            channel: facebook_message.sender.id,
-                            page: facebook_message.recipient.id
-                        };
-
-                        controller.trigger('message_delivered', [bot, message]);
-                    } else if (facebook_message.read) {
-
-                        var message = {
-                            read: facebook_message.read,
-                            user: facebook_message.sender.id,
-                            channel: facebook_message.sender.id,
-                            page: facebook_message.recipient.id,
-                            timestamp: facebook_message.timestamp,
-                        };
-
-                        controller.trigger('message_read', [bot, message]);
-                    } else if (facebook_message.referral) {
-                        var message = {
-                            user: facebook_message.sender.id,
-                            channel: facebook_message.sender.id,
-                            page: facebook_message.recipient.id,
-                            timestamp: facebook_message.timestamp,
-                            referral: facebook_message.referral,
-                        };
-
-                        controller.trigger('facebook_referral', [bot, message]);
-                    } else if (facebook_message.account_linking) {
-                        var message = {
-                            user: facebook_message.sender.id,
-                            channel: facebook_message.sender.id,
-                            timestamp: facebook_message.timestamp,
-                            account_linking: facebook_message.account_linking,
-                        };
-
-                        controller.trigger('facebook_account_linking', [bot, message]);
-                    }  else {
-                        controller.log('Got an unexpected message from Facebook: ', facebook_message);
-                    }
-                }
-            })
 		}
-    }
+	}
+}
 
 return router
 
