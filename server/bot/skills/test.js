c07cb02cb4f9170c5bbb9c056738cb0303e10d0b
jonchurch <me@jonchurch.com>
2017-07-22 22:34:49 -0400

    checkbox active working!

--- server/bot/skills/test.js
+++ server/bot/skills/test.js
@@ -4,6 +4,7 @@ module.exports = (controller)=> {
 	controller.hears('^hi$', 'message_received', (bot, message) => {
 		// find the config we need for this handler
 		const greeter = bot.config.bots.greeter
+		console.log({greeter})
 		// if greeing bot is not setup or deactivated, return true so the rest of the triggers will be evaluated
 		if (! greeter || ! greeter.active) {
 			return true
